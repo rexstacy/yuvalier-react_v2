846fff922d43e6548498efac0306a577b6094959
rexstacy <guerrerorubby@gmail.com>
2022-11-11 15:19:36 -0400

    holderjs

--- src/Components/TiendasLivedata.tsx
+++ src/Components/TiendasLivedata.tsx
@@ -8,6 +8,7 @@ function TiendasLivedata(props: tiendasDTO) {
   return (
     <Card className="text-center" style={{ width: '10rem', marginBottom: '5px', marginLeft: '10px' }}>
       <Card.Header>{props.nombre_corto}</Card.Header>
+      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
       <Card.Body>
         <Card.Title>{props.ventas}</Card.Title>
         <Card.Text>
